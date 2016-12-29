efb91e1b453bf9b78777764145eff1d43eaa816a
Sasidhar Gogulapati <sasidhar.g@ramyamlab.com>
2016-12-29 07:58:23 +0530

    Python3 Strings

---
+++ Strings.py
@@ -0,0 +1,249 @@
+
+# coding: utf-8
+
+# In[1]:
+
+'python'
+
+
+# In[2]:
+
+'this is also a string'
+
+
+# In[3]:
+
+#this is a comment
+
+
+# In[5]:
+
+"string with double quotes"
+
+
+# In[6]:
+
+'i'm using single quotes'
+
+
+# In[7]:
+
+"Now i'm using single quotes"
+
+
+# In[8]:
+
+print('hello world')
+
+
+# In[9]:
+
+print('hello \n world')
+
+
+# In[10]:
+
+'hello' \n 'world'
+
+
+# In[11]:
+
+'hello' ''\n'' 'world'
+
+
+# In[12]:
+
+'hello' '\n' 'world'
+
+
+# In[13]:
+
+print('hello')
+print('\n')
+print('world')
+
+
+# In[14]:
+
+len('hello world')
+
+
+# In[15]:
+
+str = 'hello world'
+
+
+# In[16]:
+
+str
+
+
+# In[17]:
+
+print(str)
+
+
+# In[18]:
+
+str[0]
+
+
+# In[19]:
+
+str[43]
+
+
+# In[20]:
+
+str
+
+
+# In[21]:
+
+#slicing
+
+
+# In[22]:
+
+str[1:]
+
+
+# In[23]:
+
+str[2:]
+
+str[3:]
+# In[24]:
+
+str[3:]
+
+
+# In[25]:
+
+str[:1]
+
+
+# In[26]:
+
+str[:2]
+
+
+# In[27]:
+
+str[:3]
+
+
+# In[28]:
+
+#negative indexing
+
+
+# In[29]:
+
+str[2:3]
+
+
+# In[30]:
+
+str
+
+
+# In[31]:
+
+str[:-1]
+
+
+# In[32]:
+
+str[-1:]
+
+
+# In[33]:
+
+str[::1]
+
+
+# In[34]:
+
+str[::2]
+
+
+# In[35]:
+
+str
+
+
+# In[36]:
+
+str = str + 'concatenate'
+
+
+# In[37]:
+
+str
+
+
+# In[38]:
+
+var = x
+
+
+# In[39]:
+
+var ='x'
+
+
+# In[40]:
+
+var*10
+
+
+# In[41]:
+
+var*5
+
+
+# In[42]:
+
+# String built in types
+
+
+# In[43]:
+
+str = 'hello world concatenate'
+
+
+# In[44]:
+
+str
+
+
+# In[45]:
+
+s.upper()
+
+
+# In[46]:
+
+str.upper()
+
+
+# In[47]:
+
+str.lower()
+
+
+# In[48]:
+
+str.split()
+
+
+# In[49]:
+
+str.split('w')
+
+
+# In[ ]:
+
+
+
